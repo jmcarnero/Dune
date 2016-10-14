87d932d16fb71f6215c5f0ed3c9c8075028c8c26
José M. Carnero <jm_carnero@sargazos.net>
2016-10-14 12:49:16 +0200

    corecciones

--- mods/vistas/error.php
+++ mods/vistas/error.php
@@ -5,8 +5,7 @@ $sTitle = $aMod['error']['title'];
 $sMetas = '';
 $sCss = '';
 $sJs = '';
-?>
-<div class="no_encontrado">
+?><div class="no_encontrado">
 <?php if(defined('CIERRE_APP') && CIERRE_APP){ ?><p><?php echo $this->trad('Disculpa'); ?> ...<br /><?php echo $this->trad('hemos cerrado temporalmente'); ?>.</p>
 <?php }elseif(isset($_GET['c'])){ ?><p><?php echo $this->trad('Tu sesión ha caducado'); ?> ...<br /><?php echo($this->trad('Recuerda que después de ').floor(ini_get('session.gc_maxlifetime') / 60).' '.$this->trad('minutos de').'<br />'.$this->trad('inactividad será cerrada por seguridad')); ?>.</p>
 <?php }elseif(isset($_GET['d'])){ ?><p><?php echo $this->trad('Has intentado acceder'); ?> ...<br /><?php echo $this->trad('a una sección no permitida'); ?>.</p>
