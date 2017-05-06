c93af78f32501fb1d2a9ae371aaab60307eab127
José M. Carnero <jm_carnero@sargazos.net>
2017-05-06 12:07:01 +0200

    ejemplo de modulo de error, ahora incluye mejor categorizacion de errores y recoge parametro 'tipo'

--- mods/controladores/error.php
+++ mods/controladores/error.php
@@ -32,15 +32,49 @@ class Error extends Controlazo {
 
 	function __construct(){
 		parent::__construct(__FILE__);
+		$this->sSeccion = strtolower(__CLASS__);
 
-		$this->aDatos['aMod']['error'] = array('title' => $this->trad('Error'));
+		$this->aDatos['aMod'][$this->sSeccion] = array('title' => $this->trad('Error'));
 
 		$this->aDatos['sMetas'] = '<meta name="keywords" content="Sargazos.net">';
-		$this->aDatos['sTitle'] = $this->aDatos['aMod']['error']['title'];
+		$this->aDatos['sTitle'] = $this->aDatos['aMod'][$this->sSeccion]['title'];
 		$this->aDatos['sCss'] .= '';
 		$this->aDatos['sJs'] .= '';
 
-		$this->pinta('dune', 'error');
+		$this->tipoError();
+		$this->pinta('dune', $this->sSeccion);
+	}
+
+	/**
+	 * Tipo de mensaje de error
+	 *
+	 * Espera recibir codigos de error HTTP
+	 */
+	protected function tipoError(){
+		$sTipo = empty($_GET['tipo']) ? 404 : $_GET['tipo']; //generico y seccion no encontrada
+
+		$this->aDatos['sMensaje'] = $this->trad('Disculpa'). ' ...<br />' . $this->trad('no encuentro la página que buscas');
+
+		if(defined('CIERRE_APP') && CIERRE_APP){
+			$sTipo = '503';
+		}
+
+		switch($sTipo){
+			case '401': //Unauthorized
+			case '403': //Forbidden access
+				$this->aDatos['sMensaje'] = $this->trad('Has intentado acceder') . ' ...<br />' . $this->trad('a una sección no permitida');
+				break;
+			case '408': //Request Timeout; en este caso se refiere a sesion caducada
+				$this->aDatos['sMensaje'] = $this->trad('Tu sesión ha caducado') . ' ...<br />' . $this->trad('Recuerda que después de ') . floor(ini_get('session.gc_maxlifetime') / 60) . ' ' . $this->trad('minutos de') . '<br />' . $this->trad('inactividad será cerrada por seguridad');
+				break;
+			case '503': //Service Unavailable
+				$this->aDatos['sMensaje'] = $this->trad('Disculpa') . ' ...<br />' . $this->trad('hemos cerrado temporalmente');
+				break;
+			case '404': //Not found
+			default:
+		}
+
+		$this->aDatos['sMensaje'] .= '.';
 	}
 
 }
--- mods/vistas/error.php
+++ mods/vistas/error.php
@@ -3,11 +3,8 @@
 $aMod['error'] = array('title' => $this->trad('Incidencia'));
 $sTitle = $aMod['error']['title'];
 $sMetas = '';
-$sCss = '';
+$sCss .= '<link rel="stylesheet" type="text/css" media="screen,projection" href="' . D_BASE_URL . 'css/error.css" />';
 $sJs = '';
-?><div class="no_encontrado">
-<?php if(defined('CIERRE_APP') && CIERRE_APP){ ?><p><?php echo $this->trad('Disculpa'); ?> ...<br /><?php echo $this->trad('hemos cerrado temporalmente'); ?>.</p>
-<?php }elseif(isset($_GET['c'])){ ?><p><?php echo $this->trad('Tu sesión ha caducado'); ?> ...<br /><?php echo($this->trad('Recuerda que después de ').floor(ini_get('session.gc_maxlifetime') / 60).' '.$this->trad('minutos de').'<br />'.$this->trad('inactividad será cerrada por seguridad')); ?>.</p>
-<?php }elseif(isset($_GET['d'])){ ?><p><?php echo $this->trad('Has intentado acceder'); ?> ...<br /><?php echo $this->trad('a una sección no permitida'); ?>.</p>
-<?php }else{ ?><p><?php echo $this->trad('Disculpa'); ?> ...<br /><?php echo $this->trad('no hemos encontrado la página que buscas'); ?>.</p><?php } ?>
+?><div class="error">
+	<h2><?php echo $sMensaje; ?></h2>
 </div>
